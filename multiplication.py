559e240a17f788ce0baa8b409076049efb062861
mahendhar.mothkuri <mahendhar.mothkuri@unilever.com>
2024-01-18 12:25:33 +0530

    Added Multiplication Logic

--- multiplication.py
+++ multiplication.py
@@ -1,1 +1,7 @@
-print("Multiplication code")
+print("Multiplication code\n")
+
+inp1=int(input("Enter any value\n"))
+
+inp2=int(input("Enter any value\n"))
+
+print("MULTIPLICATION OF ",inp1,"*",inp2,"=",inp1*inp2)
